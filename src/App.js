6e0cd90abe947ec86214ead48f5d9b2942b5ae7b
AnthonyIp <Ai030988>
2020-05-13 07:12:38 +0200

    handling user data in database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
 import SignInAndSignOutPage from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
-import {auth} from './firebase/firebase.utils.js';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils.js';
 
 class App extends Component {
     constructor() {
@@ -21,9 +21,28 @@ class App extends Component {
 
     componentDidMount() {
 
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user});
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            //? Check if the user Sign In
+            if (userAuth) {
+                //? Get User ref IF there is a doc ref we get it ELSE we create it
+                const userRef = await createUserProfileDocument(userAuth);
+
+                //? We are listening to the user ref if there is any change of the data
+                //? and we will also get back the first state of the data
+                userRef.onSnapshot(snapShot => {
+                    //? so then using that we are going to set the state of our local object
+                    //? with the snapshop id and data
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                })
+            } else {
+                //? if the user logs out , we set the currentUser to null
+                this.setState({currentUser: userAuth})
+            }
         })
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,13 +13,41 @@ const config = {
     measurementId: "G-VYY6Z0PTFP"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    //? if the user sign in unsuccessfully , it returns
+    if (!userAuth) return;
+
+    //? We get the user ref from the user authenthication
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    //? We tried to get the snapshot of the user
+    const snapShot = await userRef.get();
+
+    //? if the user doesn't exist , we create it
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (e) {
+            console.log('error creating user', e.message);
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt:'select_account' });
+provider.setCustomParameters({prompt: 'select_account'});
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
